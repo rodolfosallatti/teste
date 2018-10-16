ae9d570a0a1e7d2372952ac55fd0defae11c6f67
Rodolfo Sallatti <rodolfosallatti@gmail.com>
2018-10-16 11:24:20 -0300

    validation updates
    updating validation messages

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,7 +1,7 @@
 class Contato < ApplicationRecord
-    validates_presence_of :nome
-    validates_presence_of :email
-    validates_length_of :nome, minimum: 3
-    validates_length_of :email, minimum: 7
-    validates_uniqueness_of :email, message: "O E-mail já esta em uso."
+    validates_presence_of :nome, message: "O campo nome é obrigatório."
+    validates_presence_of :email, message: "O campo e-mail é obrigatório."
+    validates_length_of :nome, minimum: 3, message: "O nome deve ter no mínimo 3 caracteres."
+    validates_length_of :email, minimum: 6, message: "O E-mail deve ter no mínimo 6 caracteres."
+    validates_uniqueness_of :email, message: "O E-mail já esta cadastrado."
 end
